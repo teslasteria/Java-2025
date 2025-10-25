881944b34f3eaef0de800bfad68bce5f2f6a144f
Constantine <teslasteria@yandex.ru>
2025-10-25 10:51:21 +0300

    practice 6

---
+++ ifElseOperators.java
@@ -0,0 +1,10 @@
+package javaEducation;
+
+public class ifElseOperators {
+    public static void main(String[] args) {
+        int a = 2;
+        int i = (a == 1) ? 1 : (a == 2) ? 2 : 3;
+
+        System.out.println(i);
+    }
+}
